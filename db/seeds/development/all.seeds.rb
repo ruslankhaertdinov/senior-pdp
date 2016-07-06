555b48b5eb5104a589278f70dbd28bcb922a5a07
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2016-07-06 22:59:23 +0300

    Add articles model, update seeds

--- db/seeds/development/all.seeds.rb
+++ db/seeds/development/all.seeds.rb
@@ -2,6 +2,9 @@ FactoryGirl.create(:user, email: "user@example.com")
 
 CITIES = %w(Abakan Baksan Chelyabinsk Dalmatovo Elista Frolovo Grozny Irkutsk Kazan Leninogorsk
             Moscow Naginsk Orsk Penza Rostov Saransk Tambov Ufa Vologda Yakutsk Zainsk)
-CITIES.each do |city|
-  FactoryGirl.create(:user, email: "user-#{city.downcase}@example.com", city: city)
+CITIES.each.with_index(1) do |city, index|
+  puts "Crearing user ##{index} ..."
+  user = FactoryGirl.create(:user, email: "user-#{city.downcase}@example.com", city: city)
+  puts "Crearing user articles ..."
+  FactoryGirl.create_list(:article, 3, user: user)
 end
