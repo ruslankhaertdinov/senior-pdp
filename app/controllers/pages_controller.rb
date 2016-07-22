7c22afa7bd47dd6af407a699c5110515ace93b0c
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2016-07-23 00:12:02 +0300

    fix wrong cache invalidation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,19 +6,29 @@ class PagesController < ApplicationController
   expose(:articles) { Article.order_recent.limit(10) }
 
   def home
-    fresh_when(articles + locations)
+    fresh_when(last_modified: last_modified)
   end
 
   private
 
   def locations
-    @locations ||=
-      User.with_position.map do |user|
-        {
-          lat: user.latitude,
-          lng: user.longitude,
-          info: user.full_name
-        }
-      end
+    users_with_position.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude,
+        info: user.full_name
+      }
+    end
+  end
+
+  def users_with_position
+    @users_with_position ||= User.with_position
+  end
+
+  def last_modified
+    [
+      articles.maximum(:updated_at),
+      users_with_position.maximum(:updated_at)
+    ].max
   end
 end
