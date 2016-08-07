aa4a20f14edae976e683c9e11af649c401dab833
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2016-08-07 18:40:31 +0300

    add link to authors page

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,13 @@
 class AuthorsController < ApplicationController
   expose(:authors) { UsersByArticle.new(query_params[:query]).with_position }
+  expose(:author, model: User)
+  expose(:articles, ancestor: :author)
 
   def search
     render json: authors, each_serializer: AuthorSerializer
+  end
+
+  def show
   end
 
   private
