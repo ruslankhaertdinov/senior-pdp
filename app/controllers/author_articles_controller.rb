4ad3102a49e0763a4d1cf8381f507d7cbcc8c8ea
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2016-08-14 16:23:43 +0300

    add specs: part 2

--- app/controllers/author_articles_controller.rb
+++ app/controllers/author_articles_controller.rb
@@ -2,8 +2,44 @@ class AuthorArticlesController < ApplicationController
   before_action :authenticate_user!
 
   expose(:articles, ancestor: :current_user)
-  expose(:article)
+  expose(:article, attributes: :article_params)
 
   def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+    if article.save
+      redirect_to author_article_url(article), notice: "Article created successful!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if article.save
+      redirect_to author_article_url(article), notice: "Article updated successful!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    article.destroy
+    redirect_to author_articles_url, notice: "Article deleted successful!"
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
--- spec/features/user/articles_spec.rb
+++ spec/features/user/articles_spec.rb
@@ -14,4 +14,55 @@ feature "Articles" do
     expect(page).to have_link("Edit")
     expect(page).to have_link("Delete")
   end
+
+  scenario "Author shows own article" do
+    article = create(:article, title: "First article", user: current_user)
+
+    visit author_article_path(article)
+
+    expect(page).to have_text(article.title)
+    expect(page).to have_text(article.body)
+    expect(page).to have_link("Edit")
+    expect(page).to have_link("Delete")
+  end
+
+  scenario "Author creates article with valid params" do
+    visit new_author_article_path
+
+    fill_in "Title", with: "Awesome title"
+    fill_in "Body", with: "Great content"
+    find('input[name="commit"]').click
+
+    expect(page).to have_text("Article created successful!")
+  end
+
+  scenario "Author creates article with invalid params" do
+    visit new_author_article_path
+
+    find('input[name="commit"]').click
+
+    expect(page).to have_text("Titlecan't be blank")
+    expect(page).to have_text("Bodycan't be blank")
+  end
+
+  scenario "Author updates article" do
+    article = create(:article, title: "First article", user: current_user)
+
+    visit edit_author_article_path(article)
+
+    fill_in "Title", with: "Updated title"
+    find('input[name="commit"]').click
+
+    expect(page).to have_text("Updated title")
+    expect(page).to have_text("Article updated successful!")
+  end
+
+  scenario "Author deletes own article" do
+    article = create(:article, title: "First article", user: current_user)
+
+    visit author_article_path(article)
+    click_on("Delete")
+
+    expect(page).to have_text("Article deleted successful!")
+  end
 end
