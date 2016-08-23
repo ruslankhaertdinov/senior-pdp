d75e5a4d2ffd47098798d288af7b8989820dd198
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2016-08-23 19:05:08 +0300

    fix deprecation message

--- spec/serializers/author_serializer_spec.rb
+++ spec/serializers/author_serializer_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe AuthorSerializer do
   let(:user) { create :user }
-  let(:json) { ActiveModel::SerializableResource.new(user, serializer: described_class).to_json }
+  let(:json) { ActiveModelSerializers::SerializableResource.new(user, serializer: described_class).to_json }
   let(:user_json) { JSON.parse(json)["user"] }
 
   it "returns author" do
